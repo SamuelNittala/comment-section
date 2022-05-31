7525a67a2de9b8bfac38f1c876f356e50f90387b
SamuelNittala <nittalasamuel7@gmail.com>
2022-05-31 19:20:03 +0530

    add fixture and font.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 function App() {
-  return <div> Hello, world</div>;
+  return <div> Comment Section</div>;
 }
 
 export default App;
