6843a6176ff23a6a3d4dbbfec29e99c06a461c29
SamuelNittala <nittalasamuel7@gmail.com>
2022-06-05 18:45:56 +0530

    add immer, implement removeReply

--- src/store.ts
+++ src/store.ts
@@ -1,23 +1,36 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-param-reassign */
 import create from 'zustand';
+import { immer } from 'zustand/middleware/immer';
 import commentsData from '../public/fixtures/data.json';
 import type { CommentType, CurrentUserType } from './component/types';
 
 export type CommentState = {
   comments: Array<CommentType>;
   currentUser: CurrentUserType;
-  // eslint-disable-next-line no-unused-vars
   removeComment: (id: number) => void;
+  removeReply: (commentId: number, replyId: number) => void;
 };
 
-const useCommentStore = create<CommentState>((set) => ({
-  comments: commentsData.comments,
-  currentUser: commentsData.currentUser,
-  removeComment: (id: number) => {
-    set((state) => ({
-      ...state,
-      comments: state.comments.filter((comment) => comment.id !== id),
-    }));
-  },
-}));
+const useCommentStore = create<CommentState>()(
+  immer<CommentState>((set) => ({
+    comments: commentsData.comments,
+    currentUser: commentsData.currentUser,
+    removeComment: (id: number) => {
+      set((state: CommentState) => ({
+        ...state,
+        comments: state.comments.filter((comment) => comment.id !== id),
+      }));
+    },
+    removeReply: (commentId: number, replyId: number) => {
+      set((state: CommentState) => {
+        const commentIndex = state.comments.findIndex((comment) => comment.id === commentId);
+        state.comments[commentIndex].replies = state.comments[commentIndex].replies?.filter(
+          (reply) => reply.id !== replyId,
+        );
+      });
+    },
+  })),
+);
 
 export default useCommentStore;
